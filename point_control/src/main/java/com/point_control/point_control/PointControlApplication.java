e0de9b3958ab0cefbb781d150692e75bec071b40
IaK3lwin <krochaalmeida7@gmail.com>
2026-04-19 12:00:39 -0300

    setup project finish

---
+++ point_control/src/main/java/com/point_control/point_control/PointControlApplication.java
@@ -0,0 +1,13 @@
+package com.point_control.point_control;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PointControlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PointControlApplication.class, args);
+	}
+
+}
